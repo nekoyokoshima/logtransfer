7e0bb14da8be75c0e7cb793e4b121ce6dea88d37
Keith Goodlip <keith@eimalta.com>
2013-10-01 14:57:56 +0200

    -Remove all references to using s3fs

--- logtransfer.sh
+++ logtransfer.sh
@@ -3,27 +3,9 @@
 #Initialization
 conf=logtransfer.conf #Config file
 s3bucket=oglogs #Amazon S3 bucket name
-s3mount=/tmp/amazons3 #Mount point for Amazon bucket
 s3cmd_opts="--multipart-chunk-size-mb=1024"
 #Loading up parameters from config file
 source ./$conf
-
-#Making sure bucket is mounted
-#if grep -qs '/tmp/amazons3' /proc/mounts; then
-#	echo "It's mounted"
-#elif [ -d "$s3mount" ]; then
-#	echo "Existing folder $s3mount"
-#	echo "Mount not found!".
-#	echo "Mounting......"
-#	s3fs $s3bucket $s3mount || eval 'echo "Unable to mount S3 Bucket. Failed command s3fs $s3bucket $s3mount" 1>&2; exit 1'
-#elif [ ! -d "$s3mount" ]; then
-#	echo "Non existing folder $s3mount"
-#	echo "Creating folder"
-#	mkdir $s3mount ||  eval 'echo "Unable to create folder $3mount. Failed command mkdir $s3mount" 1>&2; exit 1'
-#	echo "Mount not found!"
-#	echo "Mounting......"
-#	s3fs $s3bucket $s3mount || eval 'echo "Unable to mount S3 Bucket. Failed command s3fs $s3bucket $s3mount" 1>&2; exit 1'
-#fi
 
 #Making sure s3cmd is installed and appropriate version
 for cmd in s3cmd; do
@@ -51,16 +33,16 @@ if [ -z $1 ]
 	echo "No Parameters passed to function"
 	return 0
 fi
+
 if [ $1 = 1 ] #Web Server Nginx Logs
 	then
 	echo "Backuptype = 1"
-	mkdir -p $s3mount/$hostname ##Create root folder if it does not exist
 	rsync -azv --human-readable --progress  --bwlimit=$bwlimit --include '*.gz' --exclude '*' /var/log/nginx/ /tmp/amazons3/$hostname/nginx
 fi
+
 if [ $1 = 2 ] #Web Server Website Logs
 	then
 	echo "Backuptype = 2"
-	mkdir -p $s3mount/$hostname/betrails ##Create Betrails folder if it does not exist
 	for d in "/var/www/"*
 		do
 			echo $d;
@@ -72,7 +54,6 @@ if [ $1 = 2 ] #Web Server Website Logs
 	#for $i in array rsync <options> /var/www/array[@] /tmp/amazons3/$hostname/betrails/array[@]
 fi
 
-
 if [ $1 = 3 ] #ABP Server Logs
 	then
 	echo "Backuptype = 3"
@@ -83,23 +64,24 @@ if [ $1 = 3 ] #ABP Server Logs
 		done
 fi
 
-
 if [ $1 = 4 ] #ABP Server Tomcat Logs
 	then
 	echo "Backuptype = 4"
 fi
+
 if [ $1 = 5 ] #MySQL Backups
 	then
 	echo "Backuptype = 5"
-fi
-if [ $1 = 6 ] #Custom folder. Path set in custom_foler in $conf
-	then
-	echo "Backuptype = 6"
 	for mysqllist in `find $mysql_path -maxdepth 1 -type f`
 		do
 			s3cmd $s3cmd_opts sync $mysql_path s3://oglogs/mysql/2013/$kukki/
 		done
 	#deletion
+fi
+
+if [ $1 = 6 ] #Custom folder. Path set in custom_foler in $conf
+	then
+	echo "Backuptype = 6"
 fi
 }
 
